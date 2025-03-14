44a9bd9936f1a6b41abbe8333f287a9870b233cb
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-03-14 16:48:51 +0100

    built various components and directed them to main-component (main Route)

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { HeaderComponent } from "../header/header.component";
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [
+    LandingPageComponent,
+    HeroComponent,
+    AboutComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    HeaderComponent,
+    ContactComponent
+],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
+
 export class MainContentComponent {
 
 }
