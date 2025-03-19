a7f4b32341a65b30aeb92931a01cb37022d6d056
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-03-19 17:10:38 +0100

    added content and styling to team.component

--- src/app/main-content/team/team.component.ts
+++ src/app/main-content/team/team.component.ts
@@ -9,4 +9,44 @@ import { Component } from '@angular/core';
 })
 export class TeamComponent {
 
+
+  teamMembers = [
+    {
+      name: 'Knecht Rubrecht',
+      project: 'Join',
+      testimonial: `had to develop, format and deliver 
+                    content in collaboration with the team members. 
+                    She is a reliable and friendly person.`,
+      background: './assets/img/team/comment1.png',
+      profile: ''
+    },
+    {
+      name: 'Knecht Rubrecht',
+      project: 'DABubble',
+      testimonial: `had to develop, format and deliver 
+                    content in collaboration with the team members. 
+                    She is a reliable and friendly person.`,
+      background: './assets/img/team/comment2.png',
+      profile: ''
+    },
+    {
+      name: 'Knecht Rubrecht',
+      project: 'general',
+      testimonial: `had to develop, format and deliver 
+                    content in collaboration with the team members. 
+                    She is a reliable and friendly person.`,
+      background: './assets/img/team/comment2.png',
+      profile: ''
+    },
+    {
+      name: 'Knecht Rubrecht',
+      project: 'Join',
+      testimonial: `had to develop, format and deliver 
+                    content in collaboration with the team members. 
+                    She is a reliable and friendly person.`,
+      background: './assets/img/team/comment1.png',
+      profile: ''
+    },
+  ]
+
 }
