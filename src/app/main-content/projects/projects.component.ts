99cb10ab8f6f22f399c8a9b2131b71f9db5e7eba
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-03-19 09:10:18 +0100

    added auto slide function

--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal} from '@angular/core';
+import { Component, inject, signal, OnInit} from '@angular/core';
 import { LanguageService } from '../../services/language.service';
 import { ProjectCardComponent } from './project-card/project-card.component';
 import { ProjectDataService } from '../../services/project-data.service';
@@ -12,11 +12,12 @@ import { ProjectDataService } from '../../services/project-data.service';
 })
 
 export class ProjectsComponent {  
-languages = inject(LanguageService);
+  languages = inject(LanguageService);
   project = inject(ProjectDataService);
   projectData = this.project.projectData;
   currentNumber = signal<number>(0)
   isHovered: boolean = false;
+  interval: number | undefined;
 
   get projectsTitle(): string {
     return this.languages.getTranslation('projects', 'title');
@@ -24,7 +25,23 @@ languages = inject(LanguageService);
 
   setCardNumber(n: number) {
     this.currentNumber.set(n);
+    clearInterval(this.interval);
+    this.startAutoSlide();  
   }
 
+  ngOnInit() {
+    this.startAutoSlide();
+  }
+
+  // stops intervals if directive, pipe or service is destroyed
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
+
+  startAutoSlide() {
+    this.interval = window.setInterval(() => {
+      this.currentNumber.update(n => (n + 1) % this.projectData.length);
+    }, 3000);
+  }
 
 }
