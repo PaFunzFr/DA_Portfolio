d150bba3a19a463f3f2d2d916f6c31957033802b
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-03-21 18:05:34 +0100

    furth adjustments to hero img hover effect

--- src/app/main-content/hero/hero.component.ts
+++ src/app/main-content/hero/hero.component.ts
@@ -11,16 +11,23 @@ export class HeroComponent {
 
 xDataLeft = 0;
 xDataRight = 0;
+xDataLeftTitle = 0;
+xDataRightTitle = 0;
 
   trackMouse(event: any) {
     const positionX = (event.clientX / window.innerWidth) * 100;
     this.xDataLeft = positionX;
     this.xDataRight = 100 - positionX;
+    const fadeFactor = Math.min(1, Math.max(0, Math.abs(positionX - 50) - 5) / 10);
+    this.xDataLeftTitle = fadeFactor * this.xDataLeft;
+    this.xDataRightTitle = fadeFactor * this.xDataRight;
   }
 
   resetPosition() {
     this.xDataLeft = 50;
     this.xDataRight = 50;
+    this.xDataLeftTitle = 0;
+    this.xDataRightTitle = 0;
   }
 
 }
