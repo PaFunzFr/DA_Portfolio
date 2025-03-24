8a47597f697d5b07ac4df1a56aa3e6adb9dbeb54
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-03-24 16:22:47 +0100

    added header to imprint component, linking routes

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { SideBarComponent } from '../shared/side-bar/side-bar.component';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, SideBarComponent],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
